0b3c44ff38f2b519f7f53d324025db29192cc9fa
tnantoka <tnantoka@bornneet.com>
2019-06-25 23:32:15 +0900

    Update chart style

--- lib/chart.dart
+++ lib/chart.dart
@@ -11,7 +11,7 @@ class _ChartState extends State<Chart> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Chart'),
+        title: const Text(''),
       ),
       body: _buildBody(),
     );
@@ -21,14 +21,14 @@ class _ChartState extends State<Chart> {
     final List<Poketype> types = Poketype.all;
     final List<String> texts = <String>[''];
     for (Poketype type in types) {
-      texts.add(type.label['ja']);
+      texts.add(type.label['ja'].substring(0, 1));
     }
     for (Poketype type in types) {
-      texts.add(type.label['ja']);
+      texts.add(type.label['ja'].substring(0, 1));
       for (double w in type.weakness.values) {
         switch (w.toString()) {
           case '0.0':
-            texts.add('');
+            texts.add('✕');
             break;
           case '0.5':
             texts.add('△');
@@ -41,6 +41,10 @@ class _ChartState extends State<Chart> {
         }
       }
     }
+    texts.add('');
+    for (Poketype type in types) {
+      texts.add(type.label['ja'].substring(0, 1));
+    }
     int i = -1;
     return Container(
       decoration: BoxDecoration(
@@ -49,21 +53,40 @@ class _ChartState extends State<Chart> {
       child: GridView.count(
         crossAxisCount: types.length + 1,
         scrollDirection: Axis.horizontal,
-        childAspectRatio: 0.4,
+        childAspectRatio: 1.0,
         children: texts.map((String text) {
           i++;
+          final bool isHeader = i < Poketype.all.length + 1 ||
+              i > texts.length - Poketype.all.length - 2 ||
+              i % (Poketype.all.length + 1) == 0;
           return Container(
             decoration: BoxDecoration(
               border: Border.all(color: Colors.black),
-              color: i % (Poketype.all.length + 1) % 2 == 1
-                  ? Colors.grey[300]
-                  : Colors.transparent,
+              color: isHeader
+                  ? text == ''
+                      ? Colors.grey[200]
+                      : Color(int.parse(
+                          'ff' +
+                              Poketype.all
+                                  .firstWhere((Poketype type) =>
+                                      type.label['ja'].startsWith(text))
+                                  .color
+                                  .substring(1),
+                          radix: 16))
+                  : i % (Poketype.all.length + 1) % 2 == 0
+                      ? Colors.grey[200]
+                      : Colors.transparent,
             ),
             child: Center(
               child: Text(
                 text,
                 style: TextStyle(
-                  fontWeight: FontWeight.bold,
+                  fontWeight: FontWeight.w900,
+                  color: isHeader
+                      ? Colors.white
+                      : text == '◯'
+                          ? Colors.green
+                          : text == '△' ? Colors.red : Colors.black,
                 ),
               ),
             ),
